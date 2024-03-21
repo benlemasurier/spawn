cbcae1e33b39970ce9afd4cd1990af288e53b74c
Ben LeMasurier <b@crypt.ly>
2024-03-21 09:53:33 -0600

    neovim: split completion cfg

---
+++ home/programs/neovim/completion.nix
@@ -0,0 +1,67 @@
+{
+  programs.nixvim = {
+    plugins = {
+      cmp-nvim-lsp.enable = true;
+      cmp-buffer.enable = true;
+      cmp-vsnip.enable = true;
+
+      cmp = {
+        enable = true;
+
+        settings = {
+          snippet.expand = ''
+            function(args)
+              vim.fn["vsnip#anonymous"](args.body)
+            end
+          '';
+
+          window = {
+            completion = {
+              winhighlight =
+                "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel";
+              scrollbar = true;
+              sidePadding = 0;
+              border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+            };
+
+            documentation = {
+              border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+              winhighlight =
+                "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel";
+            };
+          };
+
+          sources = [ { name = "nvim_lsp"; } { name = "buffer"; } ];
+
+          mapping = {
+            "<C-n>" = "cmp.mapping.select_next_item()";
+            "<C-p>" = "cmp.mapping.select_prev_item()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.close()";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<S-Tab>" =
+              "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<CR>" =
+              "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
+          };
+        };
+      };
+    };
+
+    # prevent completion for normal text
+    extraConfigLuaPost = ''
+      require('cmp').setup({
+        sources = {
+          {
+            name = 'nvim_lsp',
+            entry_filter = function(entry, ctx)
+              return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
+            end,
+          },
+        },
+      })
+    '';
+  };
+}
--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports = [ inputs.nixvim.homeManagerModules.nixvim ];
+  imports = [ ./completion.nix inputs.nixvim.homeManagerModules.nixvim ];
 
   programs.nixvim = {
     enable = true;
@@ -150,72 +150,7 @@
           };
         };
       };
-
-      # completion
-      cmp-nvim-lsp.enable = true;
-      cmp-buffer.enable = true;
-      cmp-vsnip.enable = true;
-
-      cmp = {
-        enable = true;
-
-        settings = {
-          snippet.expand = ''
-            function(args)
-              vim.fn["vsnip#anonymous"](args.body)
-            end
-          '';
-
-          window = {
-            completion = {
-              winhighlight =
-                "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel";
-              scrollbar = true;
-              sidePadding = 0;
-              border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
-            };
-
-            documentation = {
-              border = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
-              winhighlight =
-                "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel";
-            };
-          };
-
-          sources = [ { name = "nvim_lsp"; } { name = "buffer"; } ];
-
-          mapping = {
-            "<C-n>" = "cmp.mapping.select_next_item()";
-            "<C-p>" = "cmp.mapping.select_prev_item()";
-            "<C-j>" = "cmp.mapping.select_next_item()";
-            "<C-k>" = "cmp.mapping.select_prev_item()";
-            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-e>" = "cmp.mapping.close()";
-            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-            "<S-Tab>" =
-              "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-            "<CR>" =
-              "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
-          };
-        };
-      };
     };
-
-    # prevent completion for normal text
-    extraConfigLuaPost = ''
-      require('cmp').setup({
-          sources = {
-                  {
-                          name = 'nvim_lsp',
-                          entry_filter = function(entry, ctx)
-                                  return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
-                          end,
-                  },
-          },
-      })
-    '';
 
     files."ftplugin/c.lua".extraConfigLua = ''
       vim.opt.expandtab = true
