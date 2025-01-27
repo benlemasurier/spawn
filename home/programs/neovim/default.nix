653c2eb96c77b031db73f091980366735e59b9d2
Ben LeMasurier <b@crypt.ly>
2025-01-27 15:40:40 -0700

    adds override to enable himalaya-vim

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -25,11 +25,21 @@
 
     colorschemes.gruvbox.enable = true;
 
-    extraPlugins = with pkgs.vimPlugins; [ vim-vsnip vimwiki himalaya-vim ];
-
-    extraConfigVim = ''
-      let g:himalaya_folder_picker = 'telescope'
-      let g:himalaya_folder_picker_telescope_preview = 1
-    '';
+    extraPlugins = with pkgs.vimPlugins; [
+      vim-vsnip
+      vimwiki
+      (himalaya-vim.overrideAttrs (old: {
+        src = pkgs.fetchFromGitHub {
+          owner = "pimalaya";
+          repo = "himalaya-vim";
+          rev = "bb8d0ebbe82b1ba1d487354fab86d81f12a82c4d";
+          sha256 = "sha256-YW4qoCDLg/LSUKkptvecoa4n1niwZLeiSBCFLCY30j4=";
+        };
+        nvimSkipModule = [
+          "himalaya.folder.pickers.telescope"
+          "himalaya.folder.pickers.fzflua"
+        ];
+      }))
+    ];
   };
 }
