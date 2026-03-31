2d20930576a03b18bddfc60037315927f75ffd68
Ben LeMasurier <b@crypt.ly>
2026-03-30 18:05:53 -0600

    add .local/bin to path

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -16,6 +16,7 @@
       if [ "$TERM" != "dumb" ]; then
         PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
       fi
+      export PATH="$HOME/.local/bin:$PATH"
     '';
 
     shellAliases = {
